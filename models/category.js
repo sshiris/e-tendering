4d814884142a5b10077e2e104d9debc752e16d6e
AriTeil <118675580+AriTeilWork@users.noreply.github.com>
2025-03-15 17:11:15 +0200

    live server6.0

--- models/category.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const categorySchema = new mongoose.Schema({
-  category_id: { type: String, required: true, unique: true },
-  category_name: { type: String, required: true },
-  users: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
-}, { collection: 'CATEGORY' });
-
-export default mongoose.model('Category', categorySchema);
