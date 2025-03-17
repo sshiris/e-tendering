6657dfb284aa83221881f0ef35a692741d1ecef8
AriTeil <118675580+AriTeilWork@users.noreply.github.com>
2025-03-17 10:32:29 +0200

    live server bugs  fixedV1

--- models/user_category.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const userCategorySchema = new mongoose.Schema({
-  user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // Ссылка на пользователя
-  category_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true } // Ссылка на категорию
-});
-
-const UserCategory = mongoose.model('UserCategory', userCategorySchema);
-export default UserCategory;
